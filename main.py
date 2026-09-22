ffb20ad3614ecb836176c196e315c8b460f3aa14
mlepinski <mlepinski@gmail.com>
2026-09-22 18:47:56 -0400

    New Main File for Fall 2026
    Added a fresh main file for Fall 2026

---
+++ main.py
@@ -0,0 +1,17 @@
+#import all of the individual Python files
+import lepinski.py
+
+#Main Program
+def main():
+    #Dr. Matt was here
+    lepinski.dostuff()
+
+
+
+
+
+
+
+
+#Run the main program
+main()
