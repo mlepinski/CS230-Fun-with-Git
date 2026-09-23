b72282acab754fdf9a90468bae65dbc6c94ea936
dayflay <robert.flater@spartans.ut.edu>
2026-09-23 11:02:19 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #import all of the individual Python files
 import lepinski.py
+import david
 
 #Main Program
 def main():
     #Dr. Matt was here
     lepinski.dostuff()
-
+    # david says:
+    david.words_from_david()
 
 
 
