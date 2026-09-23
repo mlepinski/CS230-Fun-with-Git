b2455c78cfd17782ad8c206633c0726dd479c339
dayflay <robert.flater@spartans.ut.edu>
2026-09-23 11:00:15 -0400

    add david's stuff

---
+++ david.py
@@ -0,0 +1,4 @@
+
+def words_from_david():
+	for line in ["Although Emmy Noether was great", "I personally am a bigger fan of Ramanujan", "For his contributions to number theory", "And modern calculations of pi"]:
+		print(line)
